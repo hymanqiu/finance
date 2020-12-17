cb56b255ef111d5db628784b0cf3bb6ed3c50292
Chen Qiu <hymanq@gmail.com>
2020-12-17 16:28:00 +0100

    initial commit

---
+++ Analyzer.py
@@ -0,0 +1,13 @@
+import os
+import sys
+import math
+
+from os import rename
+
+
+def add_a_plus_b(a, b):
+    c = a + b
+    return c
+
+
+print("hello world")
---
+++ test.py
@@ -0,0 +1,7 @@
+import sys
+
+print(sys.version)
+print(sys.executable)
+
+#import Analyzer
+#print(Analyzer.add_a_plus_b(1,2))
